7718acf81532009bd7969dc275b27e9d9d54a901
W4ved <132941999+wavedshark@users.noreply.github.com>
2025-04-01 16:40:45 +0100

    Add files via upload

---
+++ workspaces/project/proyecto/consultas-bbdd.sql
@@ -0,0 +1,35 @@
+-- Obtener todos los clientes.
+-- Obtener la cantidad total de productos en todos los pedidos
+-- Obtener el precio promedio de los productos:
+-- Obtener los clientes que tienen un email válido (contiene '@'):
+-- Obtener el producto más caro.
+-- Obtener los pedidos realizados en febrero de 2024.
+-- Obtener la cantidad total de productos en todos los pedidos por producto.
+-- Obtener los clientes que han realizado más de un pedido.
+-- Obtener los productos que tienen un precio registrado.
+-- Obtener la fecha del primer pedido realizado:
+-- Obtener los productos cuyos nombres comienzan con 'A' o 'B':
+-- Obtener la cantidad total de productos en todos los pedidos por cliente ordenado por cliente.
+-- Obtener los clientes que han realizado más de un pedido en febrero de 2024.
+-- Obtener los productos con precio entre 100 y 500.
+-- Obtener la cantidad total de productos en todos los pedidos por cliente ordenado por cantidad descendente.
+-- Obtener los clientes que tienen una 'a' en cualquier posición de su nombre.
+-- Obtener el precio máximo de los productos.
+-- Obtener los pedidos realizados por el cliente con ID 1 en febrero de 2024.
+-- Obtener la cantidad total de productos en todos los pedidos por producto ordenado por total de productos descendente.
+-- Obtener los productos que no tienen un precio registrado.
+-- Obtener la fecha del último pedido realizado.
+-- Obtener los clientes cuyo nombre tiene al menos 5 caracteres.
+-- Obtener los productos que tienen la letra 'o' en cualquier posición del nombre.
+-- Obtener la cantidad total de productos en todos los pedidos por cliente ordenado por cliente.
+-- Obtener los clientes cuyos nombres no contienen la letra 'i':
+-- Obtener los pedidos realizados por el cliente con ID 2 en febrero de 2024.
+-- Obtener el precio mínimo de los productos.
+-- Obtener los clientes que han realizado al menos un pedido en febrero de 2024.
+-- Obtener la fecha del último pedido realizado por el cliente con ID 3.
+-- Obtener los productos que tienen una 'a' al final del nombre.
+-- Obtener los clientes cuyos nombres tienen al menos 4 vocales (mayúsculas|minúsculas).
+-- Obtener los productos cuyo precio tenga al menos 4 números sin contrar los decimales.
+-- Obtener los clientes cuyos nombres tienen al menos una 'a' seguida de una 'e'.
+-- Obtener los productos cuyos nombres terminan con una consonante.
+-- Obtener los productos cuyos nombres empiezan con una vocal.
